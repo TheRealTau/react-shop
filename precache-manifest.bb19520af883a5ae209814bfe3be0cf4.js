5cec754eb9579e92c8e0a5ade86c821b13ff9cd5
octavio <octaviosoto777@gmail.com>
2020-08-06 12:11:36 -0500

    Updates

---
+++ precache-manifest.bb19520af883a5ae209814bfe3be0cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c45e916567649ae487dbc08e734ffb",
+    "url": "/my-react-shop/index.html"
+  },
+  {
+    "revision": "0f0bc5bb57be6d75b2f1",
+    "url": "/my-react-shop/static/css/main.4d8c542f.chunk.css"
+  },
+  {
+    "revision": "7be2a17777b075eb4868",
+    "url": "/my-react-shop/static/js/2.f1d9e1d1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-react-shop/static/js/2.f1d9e1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0bc5bb57be6d75b2f1",
+    "url": "/my-react-shop/static/js/main.db5a6633.chunk.js"
+  },
+  {
+    "revision": "df6709c3712c04af25c8",
+    "url": "/my-react-shop/static/js/runtime-main.1945d0b0.js"
+  }
+]);
